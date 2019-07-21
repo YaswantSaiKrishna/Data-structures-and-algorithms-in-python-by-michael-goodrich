3d16cf5c3311b3cde3db3ebea6b83e0276cfb2b4
Yaswant Sai Krishna <48371142+YaswantSaiKrishna@users.noreply.github.com>
2019-07-20 23:24:36 -0700

    Python code to find weather a number n is a multiple of another number m
    Python code to find whether a number n is a multiple of another number m

--- R-1.1.py
+++ R-1.1.py
@@ -1,4 +1,4 @@
-'''Python code to find weather a number n is a multiple of another number m'''
+'''Python code to find weather a number n is a multiple of another number m '''
 def is_multiple(n,m):
     return True if ((n//m)*m==n) else False
 if __name__=='__main__':
