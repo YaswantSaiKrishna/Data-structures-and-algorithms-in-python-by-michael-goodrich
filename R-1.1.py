1cf54eac4911b21bbd677ace9d503d81dd5135ad
Yaswant Sai Krishna <48371142+YaswantSaiKrishna@users.noreply.github.com>
2019-07-20 23:18:48 -0700

    R-1.1
    Python code to find weather a number n is a multiple of another number m

---
+++ R-1.1.py
@@ -0,0 +1,5 @@
+'''Python code to find weather a number n is a multiple of another number m'''
+def is_multiple(n,m):
+    return True if ((n//m)*m==n) else False
+if __name__=='__main__':
+    print(is_multiple(33,4))
