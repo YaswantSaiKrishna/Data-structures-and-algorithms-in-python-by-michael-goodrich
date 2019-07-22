2e8cae4a8c53236be8ecf566c014278e112fdb0e
Yaswant Sai Krishna <48371142+YaswantSaiKrishna@users.noreply.github.com>
2019-07-21 19:14:54 -0700

    Update C-1.19.py

--- C-1.19.py
+++ C-1.19.py
@@ -1,3 +1,8 @@
-'''rogram to produce list containing ['a'...'z'] using list comprehension'''
+'''Program to produce list containing ['a'...'z'] using list comprehension'''
 print([chr(i) for i in range(65,91)])
 print([chr(i) for i in range(97,123)])
+'''Alternate way'''
+a=ord('A')
+b=ord('a')
+print([chr(a) for i in range(1,26)])
+print([chr(b) for i in range(1,26)])
