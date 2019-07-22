59d5f392588633587fcef274f470adae89e6bbed
Yaswant Sai Krishna <48371142+YaswantSaiKrishna@users.noreply.github.com>
2019-07-21 19:37:50 -0700

    Dot Product of 2 list
    Dot Product of 2 list

--- C-1.22.py
+++ C-1.22.py
@@ -4,5 +4,10 @@ def dot_product(a,b):
     for i in range(len(a)):
         c.append(a[i]*b[i])
     return c
+'''Alternate way
+   import numpy as np
+   c.append(np.dot(a,b))
+'''
 if __name__=='__main__':
     print(dot_product([1,2,3],[4,5,6]))
+  
